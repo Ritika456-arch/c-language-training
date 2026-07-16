97c23550e0e94c7fc6760167a98c3350409eeeb9
Ritika456-arch <twinkle.09005@gmail.com>
2026-07-16 22:20:43 +0530

    Add files via upload

---
+++ factorial with recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Recursive function to find factorial
+int factorial(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main()
+{
+    int result;
+
+    result = factorial(5);
+
+    printf("Factorial of 5 = %d", result);
+
+    return 0;
+}
